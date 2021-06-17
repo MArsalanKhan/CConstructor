fad9789a555004ea5b0f57c3b536e7a88d80c98c
Arsalan khan <45687057+MArsalanKhan@users.noreply.github.com>
2021-06-17 23:01:21 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,43 @@
+// main.c : Definiert den Einstiegspunkt f�r die Konsolenanwendung.
+//
+
+#include "stdio.h"
+
+void op1()
+{
+	printf("Operation 1\n");
+}
+
+void op2()
+{
+	printf("Operation 2\n");
+}
+
+void op3()
+{
+	printf("Operation 3\n");
+}
+
+int main()
+{
+	//A simple function pointer, returning void and passing no parameters
+	void (*fct)();
+	//Array containing 3 Function Pointers
+	void (*fctPtr[3])() = {op1,op2,op3};
+	int i;
+
+	// Calling the single pointer
+	fct = op1;	// Assigning a valid function
+	fct();		// Call the function
+
+	//Calling the array
+
+
+	for (i = 0; i<3; i++)
+	{
+		fctPtr[i]();
+	}
+
+	return 0;
+}
+
